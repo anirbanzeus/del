32f4d3aa03201bc4ab51923cbc1d92bcd096d465
Anirban Majumdar <anirbanmajumdar@Manass-iMac.local>
2019-06-25 08:13:35 +0530

    Add

---
+++ src/main/java/com/gpch/login/model/Role.java
@@ -0,0 +1,49 @@
+package com.gpch.login.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+    
+    private String selectedRole;
+
+	/**
+	 * @return the selectedRole
+	 */
+	public String getSelectedRole() {
+		return selectedRole;
+	}
+
+	/**
+	 * @param selectedRole the selectedRole to set
+	 */
+	public void setSelectedRole(String selectedRole) {
+		this.selectedRole = selectedRole;
+	}
+
+
+}
